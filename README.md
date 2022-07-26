b11c3a53df94890352ad09e649e3dea9d9f93f7c
Densuke-fitness <densukefitness2020@gmail.com>
2022-07-26 22:50:36 +0900

    add script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # greatest-common-divisor
+
+### description
+最大公約数の計算を行うshellscript
---
+++ greatest_common_divisor_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+tmp=/tmp/$$
+
+echo "---Normal Case---"
+echo "when (a > b)"
+echo "6" > $tmp-expected
+./greatest_common_divisor.sh 12 6 > $tmp-got
+diff $tmp-expected $tmp-got || echo "error: case at (a > b)" >> $tmp-error
+
+echo "when (a = b)"
+echo "13" > $tmp-expected
+./greatest_common_divisor.sh 13 13 > $tmp-got
+diff $tmp-expected $tmp-got || echo "error: case at (a = b)" >> $tmp-error
+
+echo "when (a < b)"
+echo "5" > $tmp-expected
+./greatest_common_divisor.sh 5 10 > $tmp-got
+diff $tmp-expected $tmp-got || echo "error: case at (a < b)" >> $tmp-error
+
+ERROR_EXIT () {
+    cat $tmp-error
+    rm -f $tmp-*
+    exit 1
+}
+
+if [ -f $tmp-error ]; then
+    echo "Error Occured."
+    ERROR_EXIT
+else 
+    echo "All Cases Passed."
+fi
